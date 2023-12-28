65878db6b800bc70a6a0311b2989ca8304e96d41
Dhyani <gandhidhyani@gmail.com>
2023-12-28 16:21:49 -0600

    added built-in middleware for static files

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
+let middleware = express.static(__dirname+'/public');
+app.use('/public',middleware);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/views/index.html');
