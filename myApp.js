302496c20f9f11f5f4a311a629ebb394197f1d8d
Dhyani <gandhidhyani@gmail.com>
2023-12-29 18:06:39 -0600

    chained middleware

--- myApp.js
+++ myApp.js
@@ -23,7 +23,12 @@ app.get('/json', function(req, res){
     res.json({ "message": message });
 });
 
-
+app.get('/now', function(req, res, next){
+    req.time = new Date().toString();
+    next();
+}, function(req, res){
+    res.json({"time" : req.time});
+});
 
 
 
